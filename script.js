7d6bdfb532744ca84e1f09c1bf2decdf075cbcfd
persibappsUnofficial <150852922+persibappsUnofficial@users.noreply.github.com>
2023-11-15 17:23:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["PERSIB", "PERSIBapp"],
+        typeSpeed: 100,
+        backSpeed: 70,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
